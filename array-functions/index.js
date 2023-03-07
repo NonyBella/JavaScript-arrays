8feedf8a3e88acab391a58f791ed91c82f70d9a3
Miriam Asadu <miriam.odenyi@gmail.com>
2023-03-07 15:29:02 +0100

    modified file

--- array-functions/index.js
+++ array-functions/index.js
@@ -1,26 +1,57 @@
 
- /*
-     Question 1
+/*
+    Question 1
 
-        Mutating array methods are operations that can be performed on an 
-        array that completely changes it.
+       Mutating array methods are operations that can be performed on an 
+       array that completely changes it.
 
-        Examples of methods that can mutate an array;
-        a. array.push()
-        b. array.shift()
-        c. array.splice()
-        d. array.sort()
-        e. array.unshift()
+       Examples of methods that can mutate an array;
+       a. array.push()
+       b. array.shift()
+       c. array.splice()
+       d. array.sort()
+       e. array.unshift()
 
-        Non-mutating array methods are operations that can be performed without 
-        changing the array.
+       Non-mutating array methods are operations that can be performed without 
+       changing the array.
 
-        Examples of non-mutating array methods;
-        a.  array.slice()
-        b.  array.map()
-        c.  array.filter()
-        d.  array.concat() 
- */
+       Examples of non-mutating array methods;
+       a.  array.slice()
+       b.  array.map()
+       c.  array.filter()
+       d.  array.concat() 
+*/
 
-//      Question2
- let languages =['C#','JavaScript',]
+//      Question 2a
+const languages = ['C#', 'JavaScript', 'Ruby', 'PHP', 'python'];
+languages.push('Kotlin');
+console.log(languages);
+
+//      Question 2b
+languages.splice(3, 0, 'Java');
+console.log(languages);
+
+//       Question 2c
+languages.splice(0,1);
+console.log(languages);
+
+//        Question 2d
+languages.splice(0,0,'Scala','Swift');
+console.log(languages);
+
+//        Question 2e
+languages.splice(5,1,'Go','Rust');
+console.log(languages);
+
+//        Question 3
+let fruit =['apple', 'mango','banana'];
+function changeFruit(){
+    fruit[2]='orange';
+    return fruit;
+}
+let result = changeFruit();
+result = ['apple', 'mango', 'orange'];
+
+
+
+
