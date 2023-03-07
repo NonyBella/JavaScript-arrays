2ba19e949a372f2f3249b9fc28dde9681fab2e63
Miriam Asadu <miriam.odenyi@gmail.com>
2023-03-07 14:27:24 +0100

    modified the file

---
+++ array-functions/index.js
@@ -0,0 +1,26 @@
+
+ /*
+     Question 1
+
+        Mutating array methods are operations that can be performed on an 
+        array that completely changes it.
+
+        Examples of methods that can mutate an array;
+        a. array.push()
+        b. array.shift()
+        c. array.splice()
+        d. array.sort()
+        e. array.unshift()
+
+        Non-mutating array methods are operations that can be performed without 
+        changing the array.
+
+        Examples of non-mutating array methods;
+        a.  array.slice()
+        b.  array.map()
+        c.  array.filter()
+        d.  array.concat() 
+ */
+
+//      Question2
+ let languages =['C#','JavaScript',]
